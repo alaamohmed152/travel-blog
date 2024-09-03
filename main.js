bb21d1e9b78772902ed96b45d61c6b8fa8d31d85
alaamohmed152 <161066395+alaamohmed152@users.noreply.github.com>
2024-09-03 20:18:13 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// let titleName = "Elzero" , descriptionName = "Elzero web School" , dateName="15/2";
+// let markUp = `
+// <div class = "card">
+//   <div class = "child">
+//     <h2>${titleName}</h2>
+//     <p> ${descriptionName}<\p>
+//   <\div>
+// <\div>
+// `;
+// document.write(markUp);
